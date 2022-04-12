30cc2dddfcd35e823e6241a1e33bef2d26d4c486
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2022-04-12 23:31:38 +0100

    tailwindcss

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -8,14 +8,24 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <Container fluid="true" >
-      <div className={styles.header_container}>
-        <div className={styles.arrow_box}>
-          <Image src={'/white_arrow.svg'} width={12} height={12} alt='arrow' />
-        </div>
+      <nav className={styles.header_container}>
         <div className={styles.logo}>
-          <Image src={'/logo.svg'} width={100} height={100} alt='arrow' />
+        <Image src={'/logo.svg'} width={100} height={50} alt='arrow' />
+        </div>
+          <div className={styles.header_items_middle_wrapper}>
+            <div className={styles.header_item_middle}>
+              <ul className={styles.list_wrapper}>
+                <li>Explore</li>
+                <li>Secondary Market</li>
+                <li>Supper Stay</li>
+                <li>About Us</li>
+              </ul>
+            </div>
+          </div>
+        <div className={styles.menue}>
+          <Image src={'/logo.svg'} width={100} height={50} alt='arrow' />
         </div> 
-      </div>
+      </nav>
     </Container>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Header from "../components/header/Header";
-import Login from "../components/login/Login";
 
 export default function Home() {
   return (
   <div>
     <Header />
-    <Login />
   </div>
     
   )

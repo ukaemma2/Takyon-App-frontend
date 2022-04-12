9fe3fcc99a610186608dd38544154b33f9f23282
Williams <billmal071@gmail.com>
2022-04-12 17:37:44 +0100

    feat: added tailwindcss and sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Fragment } from "react";
+import Head from "next/head";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      cacheTime: 1000 * 60 * 20,
+      staleTime: Infinity,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <title>Takyon</title>
+      </Head>
+      {/* <QueryClientProvider client={queryClient}> */}
+      <Component {...pageProps} />
+      {/* <ReactQueryDevtools initialIsOpen /> */}
+      {/* </QueryClientProvider> */}
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
